17ed1f7d51bd4ccda80290a39c10391bb82a0834
Ahmet Onur Yılmaz <ahmetonur.yilmaz@C02ZF5AALVDQ.local>
2020-05-23 13:40:00 +0300

    adds error behavior operator

--- execution_behaviour/execution_behaviour.go
+++ execution_behaviour/execution_behaviour.go
@@ -3,7 +3,6 @@ package execution_behaviour
 import (
 	"github.com/Shopify/sarama"
 	"github.com/Trendyol/kafka-wrapper/execution_behaviour/behavioral"
-	"github.com/Trendyol/kafka-wrapper/slack_api_client"
 )
 
 type BehavioralSelector interface {
@@ -11,35 +10,32 @@ type BehavioralSelector interface {
 }
 
 type retryBehaviourSelector struct {
-	executor       behavioral.BehaviourExecutor
-	logicOperator  behavioral.LogicOperator
+	normalOperator behavioral.LogicOperator
+	errorOperator  behavioral.LogicOperator
 	producer       sarama.SyncProducer
-	slackApiClient slack_api_client.Client
 	retryTopic     string
 	errorTopic     string
 	retryCount     int
-	title          string
 }
 
-func NewRetryBehaviourSelector(logicOperator behavioral.LogicOperator, producer sarama.SyncProducer, slackApiClient slack_api_client.Client, retryCount int, retryTopic, errorTopic,
-	title string) *retryBehaviourSelector {
+func NewRetryBehaviourSelector(normalOperator behavioral.LogicOperator, errorOperator behavioral.LogicOperator, producer sarama.SyncProducer,
+	retryCount int, retryTopic, errorTopic string) *retryBehaviourSelector {
 	return &retryBehaviourSelector{
-		logicOperator:  logicOperator,
+		normalOperator: normalOperator,
 		producer:       producer,
-		slackApiClient: slackApiClient,
-		title:          title,
 		retryTopic:     retryTopic,
 		errorTopic:     errorTopic,
 		retryCount:     retryCount,
+		errorOperator:  errorOperator,
 	}
 }
 
 func (r *retryBehaviourSelector) GetBehavioral(claim sarama.ConsumerGroupClaim) behavioral.BehaviourExecutor {
 	if claim.Topic() == r.retryTopic {
-		return behavioral.RetryBehavioral(r.producer, r.errorTopic, r.logicOperator, r.retryCount)
+		return behavioral.RetryBehavioral(r.producer, r.errorTopic, r.normalOperator, r.retryCount)
 	} else if claim.Topic() == r.errorTopic {
-		return behavioral.ErrorBehavioral(r.slackApiClient, r.title)
+		return behavioral.ErrorBehavioral(r.errorOperator)
 	} else {
-		return behavioral.NormalBehavioral(r.producer, r.retryTopic, r.logicOperator)
+		return behavioral.NormalBehavioral(r.producer, r.retryTopic, r.normalOperator)
 	}
 }
