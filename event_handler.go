fc99c615ef1405e746c75663af0567984cf50806
Onur Destanoğlu <onur.destanoglu@trendyol.com>
2023-04-28 11:03:23 +0300

    Onur removes the unnecessary function on the EventHandler interfacer

--- event_handler.go
+++ event_handler.go
@@ -2,11 +2,9 @@ package kafka_wrapper
 
 import (
 	"github.com/Shopify/sarama"
-	"github.com/Trendyol/kafka-wrapper/execution_behaviour"
 )
 
 type EventHandler interface {
-	BehavioralSelector() execution_behaviour.BehavioralSelector
 	Setup(sarama.ConsumerGroupSession) error
 	Cleanup(sarama.ConsumerGroupSession) error
 	ConsumeClaim(session sarama.ConsumerGroupSession, claim sarama.ConsumerGroupClaim) error
